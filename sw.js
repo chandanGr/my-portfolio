e74dce81278abd27334a4c7d844cb32c72c31734
chandan <v-grchandan@microsoft.com>
2022-04-14 19:16:55 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,20 +1,19 @@
 var self = this;
 
-const staticCacheName = 'site-static-v3';
-const dynamicCacheName = 'site-dynamic-v3';
+const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
     '/',
-    '/static/js/main.dd89474f.js',
-    '/static/js/bundle.js',
+    '/my-portfolio/static/js/bundle.js',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
     'https://fonts.googleapis.com/css?family=Concert%20One',
-    '/static/media/linkedin.f249b38224ca41a0b909.png',
-    '/static/media/github.a855c3975c028d8069f5.png',
-    '//static/media/medium.3338d2e6e2496e72be4b.png',
-    '/static/media/charts.b9d789a632ba3091d9c5.png',
-    '/static/media/category-page.a2742c748aa6dee1c941.png',
-    '/static/media/website.411b1c090a33cc178bd0.png',
-    '/static/media/zest-dashboard.533403a7907cf2b7bd1a.png'
+    '/my-portfolio/static/media/linkedin.f249b38224ca41a0b909.png',
+    '/my-portfolio/static/media/github.a855c3975c028d8069f5.png',
+    '/my-portfolio/static/media/medium.3338d2e6e2496e72be4b.png',
+    '/my-portfolio/static/media/charts.b9d789a632ba3091d9c5.png',
+    '/my-portfolio/static/media/category-page.a2742c748aa6dee1c941.png',
+    '/my-portfolio/static/media/website.411b1c090a33cc178bd0.png',
+    '/my-portfolio/static/media/zest-dashboard.533403a7907cf2b7bd1a.png'
 ];
 
 // install event
